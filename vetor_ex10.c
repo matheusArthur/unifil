831ebb83c1864c4491f0785fe1ee2a062577e0cc
matheus silva <matheusmmjm989@gmail.com>
2023-10-05 20:54:51 -0300

    mudanças no exe 10

--- vetor_ex10.c
+++ vetor_ex10.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 
 int main() {
-    const int numAlunos = 20; // Número de alunos
-    int pesquisa[numAlunos]; // Vetor para armazenar as respostas dos alunos
+    const int numAlunos = 20; 
+    int pesquisa[numAlunos]; 
 
-    // Inicialize contadores para cada resposta
-    int excelente = 0, bom = 0, regular = 0, ruim = 0, pessimo = 0;
+    int excelente = 0, bom = 0, mediano = 0, ruim = 0, horrivel = 0;
 
-    // Ler as respostas dos 20 alunos
     printf("Digite as respostas dos alunos (1 a 5):\n");
     for (int i = 0; i < numAlunos; ++i) {
         printf("Aluno %d: ", i + 1);
@@ -16,13 +14,13 @@ int main() {
         // Contar as respostas
         switch (pesquisa[i]) {
             case 1:
-                pessimo++;
+                horrivel++;
                 break;
             case 2:
                 ruim++;
                 break;
             case 3:
-                regular++;
+                mediano++;
                 break;
             case 4:
                 bom++;
@@ -32,16 +30,15 @@ int main() {
                 break;
             default:
                 printf("Resposta inválida. Digite um valor entre 1 e 5.\n");
-                i--; // Decrementar o contador para refazer a leitura
+                i--; 
                 break;
         }
     }
 
-    // Apresentar os resultados da pesquisa
     printf("\nResultado da pesquisa:\n");
-    printf("Péssimo: %d aluno(s)\n", pessimo);
+    printf("Péssimo: %d aluno(s)\n", horrivel);
     printf("Ruim: %d aluno(s)\n", ruim);
-    printf("Regular: %d aluno(s)\n", regular);
+    printf("Regular: %d aluno(s)\n", mediano);
     printf("Bom: %d aluno(s)\n", bom);
     printf("Excelente: %d aluno(s)\n", excelente);
 
